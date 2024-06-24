6349c71ed1caa3dce738d9975c34498b91dda858
Yash <yashshridhankar@kccemsr.edu.in>
2024-06-25 00:58:14 +0530

    completed

--- To-do-list-app/src/toDoList.jsx
+++ To-do-list-app/src/toDoList.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react"
 
 function ToDoList(){
 
-  const [tasks,setTasks] = useState(["STUDY","BATH","SLEEP"]);
+  const [tasks,setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event){
@@ -12,14 +12,38 @@ function ToDoList(){
   }
   function addTask(){
 
+    if(newTask.trim() !==""){
+       setTasks(t => [...t, newTask]);
+       setNewTask("");
+    }
+
   }
   function deleteTask(index){
 
-  }
-  function moveTaskUp(index){
+    const updateTasks= tasks.filter((_, i) => i !== index);
+    setTasks(updateTasks);
+
 
   }
-  function moveTaskDown(index){
+  function moveTaskUpTask(index){
+     if(index > 0){
+        const updatedtasks = [...tasks];
+        [updatedtasks[index],updatedtasks[index - 1]] =
+        [updatedtasks[index - 1],updatedtasks[index]];
+        setTasks(updatedtasks);       
+    }
+
+  }
+  function moveTaskDownTask(index){
+    
+    if(index < tasks.length -1){
+      const updatedtasks = [...tasks];
+      [updatedtasks[index],updatedtasks[index + 1]]=
+      [updatedtasks[index + 1],updatedtasks[index]];
+      setTasks(updatedtasks);
+    }
+    
+   
 
   }
 
